1a01ae0139838999d65c576b90d54a604164f6cb
slava <slava.litskevich@gmail.com>
2018-03-23 20:00:25 +0100

    REST api calls report HTTP response

--- src/com/contacts/ContactResource.java
+++ src/com/contacts/ContactResource.java
@@ -6,12 +6,14 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("contacts")
 public class ContactResource {
@@ -33,51 +35,46 @@ public class ContactResource {
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Contact getById(@PathParam("id") int id) {
-		Contact contact = findById(id);
-		return contact;
+		return findById(id);
 	}
 	
 	@POST
     @Path("")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Status insert(Contact contact) {
+    public Response insert(Contact contact) {
 		contactList.add(contact);
-        return new Status("SUCCESS", "Inserted " + contact.getEmail());
+        return Response.status(201).build();
     }
 	
 	@PUT
 	@Path("{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Status update(@PathParam("id") int id, Contact contactUpdate) {
+	public Response update(@PathParam("id") int id, Contact contactUpdate) {
 		Contact contact = findById(id);
-		if (contact != null) {
-			if (contactUpdate.getFirstName() != null) {
-				contact.setFirstName(contactUpdate.getFirstName());
-			}
-			if (contactUpdate.getLastName() != null) {
-				contact.setLastName(contactUpdate.getLastName());
-			}
-			if (contactUpdate.getEmail() != null) {
-				contact.setEmail(contactUpdate.getEmail());
-			}
-			return new Status("SUCCESS", "Updated contact with id: " + id);
-		} else {
-			return new Status("FAILURE", "Failed to find a contact with id: " + id);
-		}		
+		boolean modified = false;
+		if (contactUpdate.getFirstName() != null) {
+			contact.setFirstName(contactUpdate.getFirstName());
+			modified = true;
+		}
+		if (contactUpdate.getLastName() != null) {
+			contact.setLastName(contactUpdate.getLastName());
+			modified = true;
+		}
+		if (contactUpdate.getEmail() != null) {
+			contact.setEmail(contactUpdate.getEmail());
+			modified = true;
+		}
+		return Response.status(modified ? 200 : 304).build();
 	}
 	
 	@DELETE
 	@Path("{id}")
-	public Status delete(@PathParam("id") int id) {
+	public Response delete(@PathParam("id") int id) {
 		Contact contact = findById(id);
-		if (contact != null) {
-			contactList.remove(contact);
-			return new Status("SUCCESS", "Deleted contact with id: " + id);
-		} else {
-			return new Status("FAILURE", "Failed to find a contact with id: " + id);
-		}
+		contactList.remove(contact);
+		return Response.status(204).build();
 	}
 	
 	private Contact findById(int id) {
@@ -86,7 +83,7 @@ public class ContactResource {
 				return next;
 			}
 		}
-		return null;
+		throw new NotFoundException();
 	}
 
 	
