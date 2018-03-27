c50d1b7a17f3c346e0086637908a12540748196e
slava <slava.litskevich@gmail.com>
2018-03-27 13:06:41 +0200

    pagination support

--- src/com/contacts/ContactResource.java
+++ src/com/contacts/ContactResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,13 +14,21 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 @Path("contacts")
 public class ContactResource {
 	
-	private static final Logger LOGGER = Logger.getLogger( ContactResource.class.getName() );
+	private static final Logger LOGGER = Logger.getLogger(ContactResource.class.getName());
+	
+	private static final String PAGE_URI_FORMAT = "<%s?offset=%d&limit=%d>; rel=\"%s\"";
+	
+	private static final int DEFAULT_PAGE_LIMIT = 5;
 	
 	private static List<Contact> contactList;
 	
@@ -30,16 +39,47 @@ public class ContactResource {
 	@GET
 	@Path("")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Contact[] getAll() {
-		LOGGER.info("getAll started");
-		return contactList.toArray(new Contact[contactList.size()]);
+	public Response getAll(@Context ContainerRequestContext request, @QueryParam("offset") int offset, @QueryParam("limit") int limit) {
+		LOGGER.info("getAll started, offset: " + offset + ", limit: " + limit);
+		if (offset < 0 || offset >= contactList.size() || limit < 0) {
+			throw new BadRequestException();
+		} else {
+			int toIndex = offset + limit;
+			if (toIndex >= contactList.size()) {
+				toIndex = contactList.size() - 1; 
+			}
+			LOGGER.info("Loading contacts from " + offset + " to " + toIndex);
+			String uri = request.getUriInfo().getAbsolutePath().toString();
+			LOGGER.info("uri: " + uri);
+			
+			StringBuilder links = new StringBuilder();
+			if (offset > 0) {
+				links.append(String.format(PAGE_URI_FORMAT, uri, 0, limit, "first")).
+					  append(",").
+					  append(String.format(PAGE_URI_FORMAT, uri, offset - limit < 0 ? 0 : offset - limit, limit, "prev"));
+			}
+			if (offset + limit < contactList.size()) {
+				if (links.length() > 0) {
+					links.append(",");
+				}
+				links.append(String.format(PAGE_URI_FORMAT, uri, contactList.size() - limit, limit, "last")).
+					  append(",").
+					  append(String.format(PAGE_URI_FORMAT, uri, offset + limit, limit, "next"));
+			}
+
+			LOGGER.info("resources. links: " + links.toString());
+			List<Contact> result = contactList.subList(offset, toIndex);
+			return Response.ok(result.toArray(new Contact[result.size()])).
+							header("Links", links.toString()).
+							build();
+		}
 	}
 	
 	@GET
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Contact getById(@PathParam("id") int id) {
-		return findById(id);
+	public Response getById(@PathParam("id") int id) {
+		return Response.ok(findById(id)).build();
 	}
 	
 	@POST
